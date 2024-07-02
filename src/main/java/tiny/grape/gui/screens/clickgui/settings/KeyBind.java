d50294746431d95327e91931350aed33a2aae5bc
grape-wtf <iammrgrape@gmail.com>
2024-07-02 02:00:18 -0500

    Convers number to letter

--- src/main/java/tiny/grape/gui/screens/clickgui/settings/KeyBind.java
+++ src/main/java/tiny/grape/gui/screens/clickgui/settings/KeyBind.java
@@ -4,6 +4,7 @@ import net.minecraft.client.gui.DrawContext;
 import tiny.grape.gui.screens.clickgui.ModuleButton;
 import tiny.grape.module.settings.KeyBindSetting;
 import tiny.grape.module.settings.SettingsHandler;
+import org.lwjgl.glfw.GLFW;
 
 import java.awt.*;
 
@@ -31,12 +32,12 @@ public class KeyBind extends Component {
             binding.setKey(key);
             isBinding = false;
         }
-        if ((binding.getKey() == 256)) {
+        if ((binding.getKey() == 256)) { // Escape key
             parent.module.setKey(0);
             binding.setKey(0);
             isBinding = false;
         }
-        if ((binding.getKey() == 259)) {
+        if ((binding.getKey() == 259)) { // Backspace key
             parent.module.setKey(0);
             binding.setKey(0);
             isBinding = false;
@@ -49,9 +50,18 @@ public class KeyBind extends Component {
         context.fill(parent.parent.x, parent.parent.y + parent.offset + offset, parent.parent.x+parent.parent.width, parent.parent.y + parent.offset + offset + parent.parent.height, new Color(0,0,0, 160).getRGB());
         int offset2 = ((parent.parent.height/2) - client.textRenderer.fontHeight/2);
 
-        if (isBinding==false) context.drawText(client.textRenderer, "Keybind: " + binding.getKey(), parent.parent.x + offset2, parent.parent.y + parent.offset + offset + offset2, Color.white.getRGB(), true);
-        if (isBinding==true) context.drawText(client.textRenderer, "Binding: " + binding.getKey(), parent.parent.x + offset2, parent.parent.y + parent.offset + offset + offset2, Color.white.getRGB(), true);
+        String keyName = getKeyName(binding.getKey());
+        if (isBinding == false) context.drawText(client.textRenderer, "Keybind: " + keyName, parent.parent.x + offset2, parent.parent.y + parent.offset + offset + offset2, Color.white.getRGB(), true);
+        if (isBinding == true) context.drawText(client.textRenderer, "Binding: " + keyName, parent.parent.x + offset2, parent.parent.y + parent.offset + offset + offset2, Color.white.getRGB(), true);
 
         super.render(context, mouseX, mouseY, delta);
+    }
+
+    private String getKeyName(int keyCode) {
+        // Return None if key is 0
+        if (keyCode == 0) return "None";
+        String keyName = GLFW.glfwGetKeyName(keyCode, 0);
+        // Return Unknown if key is <0
+        return keyName != null ? keyName.toUpperCase() : "Unknown";
     }
 }
